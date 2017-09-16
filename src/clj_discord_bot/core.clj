a48cbcee6b3c42d7fe85e719bd424d29c2b470ae
Dan Harasymiw <danharasymiw@gmail.com>
2017-09-16 18:37:26 -0400

    add help command

--- src/clj_discord_bot/core.clj
+++ src/clj_discord_bot/core.clj
@@ -9,14 +9,18 @@
 
 (defn generate-img-search-url [query] (str "https://www.googleapis.com/customsearch/v1?q=" query "&cx=007505347843268886703%3Asukibcfg6xq&num=1&searchType=image&start=" (+ (rand-int 100) 1)  "&key=" google-token))
 
-(defn d100 [type data]
-  (discord/answer-command data "!d100" (str "You rolled: " (+ (rand-int 100) 1))))
+(defn d20
+      "!d20 - Picks a random number from 1-20"[type data]
+      (discord/answer-command data "!d20" (str "You rolled: " (+ (rand-int 100) 1))))
 
 (defn img-search [query]
       (let [result (http-client/get (generate-img-search-url query) {:as :json})]
            (get (nth (get-in result [:body :items]) 0) :link)))
 
-(defn find-img [type data]
+(defn find-img
+      "!img <your_query> - Finds a random image of <your_query>"
+      [type data]
+
       (let [message (get data "content")]
            (if (.startsWith message "!img")
              (let [split-string (clojure.string/split message #" ")]
@@ -27,18 +31,28 @@
       (Thread/sleep (* (+ (rand-int 5) 1) 60 1000))
       (discord/answer-command data message (img-search "quaggan")))
 
-(defn quaggan-joe [type data]
-      (let [message (get data "content")
-            mentions (get data "mentions")
-            mention-all (get data "mention_everyone")]
-
-           (if (or mention-all (.contains message quaggan-token))
-             (quaggan-answer data message))))
+;(defn quaggan-joe [type data]
+;      (let [message (get data "content")
+;            mentions (get data "mentions")
+;            mention-all (get data "mention_everyone")]
+;
+;           (if (or mention-all (.contains message quaggan-token))
+;             (quaggan-answer data message))))
 
 (defn gandhi-spellcheck [type data]
       (let [message (get data "content")]
            (if (re-find #"(?i)ghandi" message)
              (discord/answer-command data message "Gandhi  (╯°□°）╯︵ ┻━┻"))))
+
+(defn help [type data]
+      (let [message (get data "content")]
+           (print "helpin")
+           (if (.equals "!help" message)
+             (discord/answer-command data message
+                                     (str "Commands \n"
+                                          (:doc (meta #'d20)) "\n"
+                                          (:doc (meta #'find-img)) "\n"
+                                          (:doc (meta #'help)))))))
 
 
 (defn log-event [type data] 
@@ -46,8 +60,8 @@
 
 (defn -main [& args]
   (discord/connect discord-token
-                   {"MESSAGE_CREATE" [d100 find-img quaggan-joe gandhi-spellcheck]
-                    "MESSAGE_UPDATE" [d100]
+                   {"MESSAGE_CREATE" [d20 find-img gandhi-spellcheck help]
+                    "MESSAGE_UPDATE" [d20]
                     ; "ALL_OTHER" [log-event]
                     }
                    true))
